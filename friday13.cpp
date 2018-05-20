abc040f8dcdc80624c55908957dccdebe6a7a18e
Brian Ulrich <briandulrich@gmail.com>
2018-05-19 20:14:12 -0400

    Updated GPL header
    Update GPL header to correct version (v3)

--- friday13.cpp
+++ friday13.cpp
@@ -7,22 +7,25 @@ Most of the date-calculating code comes from "calendar_gen_III - Copy - Copy.cpp
 	on the theory and operation of slide rules.) :)
 */
 
-/*
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
+/* 
+ * Copyright 2018 Brian Ulrich
  * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * This file is part of friday13.
  * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
- * MA 02110-1301, USA.
+ * friday13 is free software: you can redistribute it and/or 
+ * modify it under the terms of the GNU General Public License 
+ * as published by the Free Software Foundation, either 
+ * version 3 of the License, or (at your option) any later 
+ * version.
  * 
+ * friday13 is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty 
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See 
+ * the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public 
+ * License along with friday13. If not, see 
+ * http://www.gnu.org/licenses/.
  */
 
 #include <iostream>
